585481c5af66e27fe0ad27a1775ea52e0c204007
chiba <62425462+TranDuy13@users.noreply.github.com>
2023-06-03 23:10:50 +0700

    update BG css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./components/login/Login";
-import Forgot from "./components/login/Forgot";
-import Admin from "./components/Admin";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Forgot from "./pages/login/Forgot";
+import Product from "./components/Product/Product";
 
 const Auth = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path='/admin' component={Admin} />
-        <Route path='/login' component={Login} />
-        <Route path='/forgot-password' component={Forgot} />
-        <Route path='/' component={Login} />
-        
-      </Switch>
-    </Router>
- 
-  );
-}
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/forgot-password" element={<Forgot />} />
+                <Route path="/home" element={<Product />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default Auth;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Product.css";
+function Product() {
+  return (
+    <>
+      <div>
+        <div class="images">
+          <div class="image-item">
+            <img
+              src="https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              alt=""
+            />
+          </div>
+          <div class="image-item">
+            <img
+              src="https://images.unsplash.com/photo-1431794062232-2a99a5431c6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80"
+              alt=""
+            />
+          </div>
+          <div class="image-item">
+            <img
+              src="https://images.unsplash.com/photo-1446329813274-7c9036bd9a1f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              alt=""
+            />
+          </div>
+          <div class="image-item">
+            <img
+              src="https://images.unsplash.com/photo-1444464666168-49d633b86797?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80"
+              alt=""
+            />
+          </div>
+          <div class="image-item">
+            <img
+              src="https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              alt=""
+            />
+          </div>
+          <div class="image-item">
+            <img
+              src="https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              alt=""
+            />
+          </div>
+          <div class="image-item">
+            <img
+              src="https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Product;
